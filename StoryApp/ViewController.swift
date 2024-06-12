36a0072248b7cfcdefcd4b67c12a2d490732d91a
soyung sim <simsy1024@gmail.com>
2024-06-12 14:06:09 +0900

    Initial Commit

---
+++ StoryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryApp
+//
+//  Created by 심소영 on 6/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
